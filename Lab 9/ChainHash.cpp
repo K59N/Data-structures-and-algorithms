ba0b23c0c8d73e3daaa5f3c559f77a67bfc1942b
Loc VU <locvx1234@gmail.com>
2015-11-11 02:33:45 +0700

    Add 2 function readFile and findInFile

--- Lab 9/ChainHash.cpp
+++ Lab 9/ChainHash.cpp
@@ -1,4 +1,4 @@
-//! @Filename ThucHanh08_ADT_Binary_Search_Tree.cpp
+//! @Filename ChainHash.cpp
 //! @Author LocVU
 //! @Creaton 11.11.15
 #include <iostream>
@@ -8,27 +8,30 @@ using namespace std;
 
 class ChainHash{
     public:
-        static const int SIZE = 31;
+        static const int SIZE = 31;                 // Kich thuoc bang bam - So nguyen to thoa man 4k+3
         ChainHash();
 //        ChainHash(const ChainHash & Table);
 //        ~ChainHash();
 //        void operator=(const ChainHash &Table);
-        bool search(string dict);
-        void insert(const string & dict);
-        void print();
+        bool search(string dict);                   // Tim dict co trong bang bam hay khong
+        void insert(const string & dict);           // Chen dict tu tu dien vao bang bam
+        void print();                               // In bang bam
+        void readFile(const char * filename);       // Doc tu dien
+        void findInFile(const char * filename);     // Tim tu trong file input
     private:
-        struct Node{
+        struct Node{            // Cau truc 1 Node trong slot
             string data;
             Node *next;
         };
         Node * T[SIZE];
         unsigned int Hash(string dict);
+        string str;
 };
 
 
 ChainHash::ChainHash(){
     for(int i = 0; i < SIZE; i++){
-        T[i] = NULL;
+        T[i] = NULL;                    // Khoi tao Node dau tien cua cac Slot la NULL
     }
 }
 
@@ -55,7 +58,7 @@ void ChainHash::insert(const string &dict){
     if(p != NULL){
         p->data = dict;
         p->next = T[i];
-        T[i] = p;  //Xen vao dau day chuyen
+        T[i] = p;                        //Xen vao dau day chuyen
     }else{
         cout << "Tu " << dict << "da xuat hien o slot " << i << endl;
     }
@@ -71,38 +74,45 @@ void ChainHash::print(){
     }
 }
 
-int main(){
-    string dict, str;
-    ChainHash chh;
-    ifstream infile("dictionary.txt");
+void ChainHash::readFile(const char * filename){
+    ifstream infile(filename);
     if(!infile.good()){
-        cout << "dictionary.txt doesn't exist !" << endl;
+        cout << filename << " doesn't exist !" << endl;
         cin.ignore();
         cin.get();
     }
     while(!infile.eof()){
         getline(infile, str, ' ');
-        chh.insert(str);
+        insert(str);
     }
     infile.close();
+}
+
+void ChainHash::findInFile(const char * filename){
+    ifstream infile(filename);
+    if(!infile.good()){
+        cout << filename << " doesn't exist !" << endl;
+        cin.ignore();
+        cin.get();
+    }
+    while(!infile.eof()){
+        getline(infile, str, ' ');
+        if(!search(str)){
+            cout << str << endl;
+        }
+    }
+    infile.close();
+}
+
+int main(){
+    string dict, str;
+    ChainHash chh;
+    chh.readFile("dictionary.txt");
 
     cout << "Bang bam :  " << endl;
     chh.print();
     cout << "Tim kiem cac tu trong file input.txt khong co trong tu dien \n" << endl;
-
-    infile.open("input.txt");
-    if(!infile.good()){
-        cout << "input.txt doesn't exist !" << endl;
-        cin.ignore();
-        cin.get();
-    }
-    while(!infile.eof()){
-        getline(infile, str, ' ');
-        if(!chh.search(str)){
-            cout << str << endl;
-        }
-    }
-     infile.close();
+    chh.findInFile("input.txt");
 
     return 0;
 }
